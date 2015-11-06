3afacc87594c9d1332d5b4f345e9d74e20c5c457
Ian Cronkright <txurtian@yahoo.com>
2015-11-05 20:19:25 -0500

    Update notification.js

--- plugins/txuritan/notification.js
+++ plugins/txuritan/notification.js
@@ -1,1 +1,18 @@
 Notification.requestPermission();
+function notifyMe() {
+  if (!("Notification" in window)) {
+    alert("This browser does not support desktop notification");
+  }
+  else if (Notification.permission === "granted") {
+    var notification = new Notification("Hi there! Notifications will now work and the button should disaper.");
+    $("#notify").remove();
+  }
+  else if (Notification.permission !== 'denied') {
+    Notification.requestPermission(function (permission) {
+      if (permission === "granted") {
+        var notification = new Notification("Hi there! Notifications will now work and the button should disaper.");
+        $("#notify").remove();
+      }
+    });
+  }
+}
